6dd12912164d8d65e0159ed03b51b3394a10dbe9
forrestbthomas <forrestbthomas@gmail.com>
2014-09-26 08:49:10 -0700

    feature(ejs): Change rules view to display only one tab

--- public/dist/javascript-battle-website.js
+++ public/dist/javascript-battle-website.js
@@ -403,14 +403,12 @@ var Game = Backbone.Model.extend({
   
   initialize: function(){
     this.viewing = {};
-    this.viewing = "general";
+    this.viewing = "rules";
     this.render();
   },
 
   events: {
-    'click .general': 'showGeneral',
-    'click .rules': 'showRules',
-    'click .improve': 'showImprove'
+    'click .rules': 'showRules'
   },
 
   showRules: function(event) {
@@ -419,30 +417,12 @@ var Game = Backbone.Model.extend({
     this.render();
     $('.rules').tab('show');
   },
-  
-   showGeneral: function(event) {
-    event.preventDefault();
-    this.viewing = "general";
-    this.render();
-    $('.general').tab('show');
-  },
-
-   showImprove: function(event) {
-    event.preventDefault();
-    this.viewing = "improve";
-    this.render();
-    $('.improve').tab('show');
-  },
 
   render: function(){
     var html;
     if(this.viewing === "rules") {
       html = new EJS({url: '/ejs_templates/rules'}).render(this.model);
-    } else if (this.viewing === "general") {
-      html = new EJS({url: '/ejs_templates/general'}).render(this.model);
-    }  else if (this.viewing === "improve") {
-      html = new EJS({url: '/ejs_templates/improve'}).render(this.model);
-    }
+    } 
     this.$el.html(html);
   }
 
