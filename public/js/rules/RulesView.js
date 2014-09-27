5907ce4dbd48b46e3dbebdd271e7fbdd172ea91c
forrestbthomas <forrestbthomas@gmail.com>
2014-09-27 13:18:17 -0700

    feature(public): Add functionality to grab user code after file upload

--- public/js/rules/RulesView.js
+++ public/js/rules/RulesView.js
@@ -3,11 +3,14 @@ var RulesView = Backbone.View.extend({
   initialize: function(){
     this.viewing = {};
     this.viewing = "rules";
+    console.log(this.model)
     this.render();
   },
 
   events: {
-    'click .rules': 'showRules'
+    'click .rules': 'showRules',
+    'change #hero': 'getFiles',
+    'click #helper': 'getOther'
   },
 
   showRules: function(event) {
@@ -23,6 +26,21 @@ var RulesView = Backbone.View.extend({
       html = new EJS({url: '/ejs_templates/rules'}).render(this.model);
     } 
     this.$el.html(html);
+  },
+
+  getFiles: function() {
+    var reader = new FileReader();
+    var file = this.$el.find('#hero')[0].files[0];
+    var that = this;
+    reader.onload = function(e) {
+      that.model.set('heroCode', reader.result);
+    };
+    reader.readAsText(file);
+
+  },
+
+  getOther: function() {
+    console.log(this.model.get('heroCode'));
   }
 
 
