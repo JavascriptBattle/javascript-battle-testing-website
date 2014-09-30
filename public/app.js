a3d8c6052234e6111d02e2db98d903595a10f36b
forrestbthomas <forrestbthomas@gmail.com>
2014-09-30 16:01:02 -0700

    fix(all): Remove various files

--- public/app.js
+++ public/app.js
@@ -10,10 +10,6 @@ app.game.helpers = require('./helpers.js');
 app.gameView = new GameView({ model: app.game });
 $('.gamegrid-content').append(app.gameView.$el);
 
-app.user = new User();
-app.userView = new UserView({ model: app.user });
-$('#join').append(app.userView.$el);
-
 app.navbarView = new NavbarView({ model: app.user });
 $('.navbar').append(app.navbarView.$el);
 
