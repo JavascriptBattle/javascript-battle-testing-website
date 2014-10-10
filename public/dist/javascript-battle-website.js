1a18eb729e49038f1c78703e40a2768f4718b512
forrestbthomas <forrestbthomas@gmail.com>
2014-10-10 10:03:37 -0700

    feature(public): Abstract out file upload

--- public/dist/javascript-battle-website.js
+++ public/dist/javascript-battle-website.js
@@ -114,13 +114,13 @@ var Game = Backbone.Model.extend({
   },
 
   runGame: function() {
-    if (this.get('heroCode') === undefined) {
+    if (this.get('hero') === undefined) {
       alert('Please upload your Hero.js file first.');
       return 'Error';
     } else {
       this.waiting = true;
 
-      var move = this.get('heroCode');
+      var move = this.get('hero');
       var end = move.indexOf('module.exports = move;', move.length - 25);
       move = move.slice(0, end);
       move += "\n return move(arguments[0], arguments[1]);";
@@ -494,7 +494,7 @@ var Game = Backbone.Model.extend({
 
   events: {
     'click .simulate': 'simulate',
-    'change #hero': 'getHeroCode'
+    'change #hero': 'getCode'
   },
 
   simulate: function() {
@@ -572,15 +572,15 @@ var Game = Backbone.Model.extend({
     }
   },
 
-  getHeroCode: function() {
+  getCode: function(heroOrHelper) {
     var reader = new FileReader();
-    var heroCode = this.$el.find('#hero')[0].files[0];
+    var code = heroOrHelper.currentTarget.files[0];
     var that = this;
     reader.onload = function(e) {
-      that.model.set('heroCode', reader.result);
-      console.log('Hero code has been saved.\nNo need to re-upload, unless you have changed your file.');
+      that.model.set(heroOrHelper.currentTarget.id, reader.result);
+      console.log(heroOrHelper.currentTarget.id + ' code has been saved.\nNo need to re-upload, unless you have changed your file.');
     };
-    reader.readAsText(heroCode);
+    reader.readAsText(code);
 
   }
 
